9d6cfcc70d1100ae982277958f2db85852900802
ovis1me1me <126409777+ovis1me1me@users.noreply.github.com>
2023-04-04 12:31:57 +0900

    Create get_num_squre.py

---
+++ get_num_squre.py
@@ -0,0 +1,12 @@
+#src/get_num_square.py
+import os
+
+num = os.environ.get("INPUT_NUM")
+if num:
+  try:
+    num=int(num)
+   except Exception:
+    exit()
+else:
+  num=1
+print(f"::set-output name=num_squared::{num**2}")
